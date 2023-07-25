ed4a45524fa7141eff761c313de2d7834958dc3d
phillychi3 <phillychi3@gmail.com>
2023-07-26 01:25:40 +0800

    sync

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import logo from './logo.svg';
-import GridLayout from "react-grid-layout";
 import Search from "./things/search.js";
 import './App.css';
+import RGL, { WidthProvider } from "react-grid-layout";
+
+const ReactGridLayout = WidthProvider(RGL);
 
 function App() {
+  // const layouts = [];
   const layouts = [
-    { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 }
+      { i: "a", x: 0, y: 0, w: 4, h: 1 },
+      { i: "b", x: 4, y: 0, w: 4, h: 1 },
+      { i: "c", x: 8, y: 0, w: 4, h: 1 },
   ];
   return (
     <div className="App">
       <img id='mianbg' src="https://cdn.discordapp.com/attachments/1005190123073318923/1099345993587232913/CTEC_Sort.png" className="App-logo" alt="logo" />
       <div class="dontclickimage" id="dontclickimage"></div>
-      <GridLayout
+      <ReactGridLayout
         className="layout"
         layout={layouts}
-        cols={12}
-        rowHeight={30}
-        width={1200}
+        breakpoints={1200}
+        cols={12} 
       >
-        <div key="a">
-          <Search/>
-        </div>
-      </GridLayout>
+        <div key="a"><Search/></div>
+        <div key="b"><Search/></div>
+        <div key="c"><Search/></div>
+      </ReactGridLayout>
+      
     </div>
   );
 }
--- src/things/search.js
+++ src/things/search.js
@@ -2,14 +2,35 @@ import React from "react";
 
 
 class Search extends React.Component {
+    // usestate
+    constructor(props) {
+        super(props);
+        this.state = {
+            search: "",
+        };
+    }
+
+
+    getinput = (thing) => {
+        console.log(thing.target.value);
+        this.setState({ search: thing.target.value });
+    }
+    getenter = (thing) => {
+        if (thing.key === "Enter") {
+            console.log("Enter");
+            console.log(this.state.search);
+            //redirect to search page : googld
+            window.location.href = "https://www.google.com/search?q=" + this.state.search;
+
+        }
+    }
+
     render() {
         return (
             <div class="search-thing" id="search-thing">
-                <input type="text" placeholder="Search for a game" />
-                <div class="search-button" id="search-button">
-                </div>
-                <div id="change-search-button">
-                </div>
+                <input type="text" placeholder="Search for a game" onChange={this.getinput} onKeyDown={this.getenter}></input>
+                <div class="search-button" id="search-button"></div>
+                <div id="change-search-button"></div>
             </div>
         );
     }
