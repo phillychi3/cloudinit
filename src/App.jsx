7df89695ec35154f1ee436f596316ef6ffeeae04
phillychi3 <phillychi3@gmail.com>
2024-03-02 14:17:11 +0800

    feat: add bg

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Search from './components/Search'
 function App() {
   return (
     <>
+      <img id="bg" src="https://wallpapers.com/images/hd/minimalist-purple-sky-and-mountain-ex4suuw5xd4funov.jpg" alt="background" />
+      <div className='mask'></div>
       <div className="App">
         <Search />
       </div>
