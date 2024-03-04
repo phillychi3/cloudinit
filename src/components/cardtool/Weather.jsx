5f487f6ce32c74991daf5a3c9fb76d3c71298630
phillychi3 <phillychi3@gmail.com>
2024-03-04 21:21:34 +0800

    weather 75%

--- src/components/cardtool/Weather.jsx
+++ src/components/cardtool/Weather.jsx
@@ -2,7 +2,6 @@ import useLocation from '../../hooks/uselocation'
 import useWeather from '../../hooks/useweather'
 
 const Weathercard = (data) => {
-  console.log(data)
   return (
     <div className="weather-card">
       <span>
@@ -13,9 +12,25 @@ const Weathercard = (data) => {
         alt={data.weather.weather[0].description}
       />
       <div className="weather-description">
-        <p>{data.weather.main.temp}°C</p>
+        <p>{data.weather.main.temp.toFixed(1)}°C</p>
         <p>濕度{data.weather.main.humidity}</p>
         <p>降雨機率</p>
+      </div>
+      <div className="weather-forecast">
+        {Object.keys(data.forecast).map((day) => (
+          <div key={day} className="weather-forecast-day">
+            <span>{day}</span>
+            <img
+              src={`http://openweathermap.org/img/wn/${data.forecast[day].icon}.png`}
+              alt={data.forecast[day].description}
+            />
+            <span>
+              {data.forecast[day].max}°C/{data.forecast[day].min}°C
+            </span>
+            <span>降雨機率{data.forecast[day].rain}%</span>
+          </div>
+        ))
+        }
       </div>
     </div>
   )
--- src/hooks/useweather.js
+++ src/hooks/useweather.js
@@ -3,8 +3,9 @@ import React from 'react'
 const forecast = `https://api.openweathermap.org/data/2.5/forecast?lat=23.701156&lon=120.5405259&units=metric&appid=ffd9818a303f06784b81bc37127cf92c`
 const weather = `https://api.openweathermap.org/data/2.5/weather?lat=23.701156&lon=120.5405259&units=metric&appid=ffd9818a303f06784b81bc37127cf92c#`
 
-const processdate = (date) => {
-}
+//將資料分成一天
+//一天內的最高與最低溫紀錄下來
+//平均一天的降雨機率
 
 export default function useWeather(location) {
   const [state, setState] = React.useState({
@@ -13,11 +14,43 @@ export default function useWeather(location) {
     forecast: null,
     error: null
   })
+  const processdate = (date) => {
+    let days = {}
+    for (let i = 0; i < date.list.length; i++) {
+      let d = new Date(date.list[i].dt * 1000)
+      let day = `${d.getUTCFullYear()}-${d.getUTCMonth() + 1}-${d.getUTCDate()}`
+      if (!days[day]) {
+        days[day] = []
+      }
+      days[day].push(date.list[i])
+    }
+    let rtdata = {}
+    for (let day in days) {
+      const { max, min, rain } = days[day].reduce((acc, curr) => {
+        return {
+          max: Math.max(acc.max, curr.main.temp_max),
+          min: Math.min(acc.min, curr.main.temp_min),
+          rain: acc.rain + curr.pop*100,
+        };
+      }, { max: -100, min: 100, rain: 0});
 
+      rtdata[day] = {
+        max: max.toFixed(1),
+        min: min.toFixed(1),
+        rain: (rain / days[day].length).toFixed(1),
+        icon: days[day][0].weather[0].icon,
+      };
+    }
+    return rtdata
+
+  }
   React.useEffect(() => {
-    const success = (forecast,weather) => {
-      console.log(forecast,weather)
-      if (forecast.cod.toString() !== '200' || weather.cod.toString() !== '200') {
+    const success = (forecast, weather) => {
+      console.log(forecast, weather)
+      if (
+        forecast.cod.toString() !== '200' ||
+        weather.cod.toString() !== '200'
+      ) {
         setState((s) => ({
           ...s,
           loading: false,
@@ -25,10 +58,21 @@ export default function useWeather(location) {
         }))
         return
       }
+      try {
+        forecast = processdate(forecast)
+      }
+      catch (e) {
+        setState((s) => ({
+          ...s,
+          loading: false,
+          error: 'process error'
+        }))
+        return
+      }
       setState({
         loading: false,
         weather: weather,
-        forecast: forecast
+        forecast: forecast,
       })
     }
     const efrror = (error) => {
@@ -39,18 +83,14 @@ export default function useWeather(location) {
       }))
     }
     if (!location.loading && !location.error) {
-      Promise.all([
-        fetch(forecast),fetch(weather)
-      ])
-      .then(([res1,res2]) => {
-        return Promise.all([res1.json(),res2.json()])
-      }
-      )
-      .then(([forecast,weather]) => {
-        success(forecast,weather)
-      })
-      .catch(efrror)
-
+      Promise.all([fetch(forecast), fetch(weather)])
+        .then(([res1, res2]) => {
+          return Promise.all([res1.json(), res2.json()])
+        })
+        .then(([forecast, weather]) => {
+          success(forecast, weather)
+        })
+        .catch(efrror)
     }
   }, [location])
   return state
