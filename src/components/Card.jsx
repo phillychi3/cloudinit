951cf25c6f5234c92595f3b14e99534e4c0beb60
phillychi3 <phillychi3@gmail.com>
2024-04-24 01:20:22 +0800

    add setting modal

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,36 @@
 import PropTypes from 'prop-types'
 import { useEffect, useState } from 'react'
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root')
+
 const Card = ({ element, color }) => {
   const [backgroundColor, setBackgroundColor] = useState('#ecf0f1')
   const [ishover, setIshover] = useState(false)
+  const [opencardsetting, setcardopen] = useState(false)
+
+
+  const Style = {
+    content: {
+      inset: '50% auto auto 50%',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      width: '50%',
+      height: '50%',
+      backgroundColor: 'white',
+      borderRadius: '10px',
+      boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)'
+    }
+  }
+
+  function switchModal() {
+    setIshover(false)
+    setcardopen(!opencardsetting)
+  }
+
+  function closeModal() {
+    setcardopen(false)
+  }
 
   useEffect(() => {
     if (color) {
@@ -11,24 +39,33 @@ const Card = ({ element, color }) => {
   }, [color])
 
   return (
-    <div
-      className="card"
-      style={{
-        borderColor: backgroundColor,
-        borderStyle: color ? 'solid' : 'none'
-      }}
-      onMouseEnter={() => setIshover(true)}
-      onMouseLeave={() => setIshover(false)}
-    >
-      {ishover && (
-        <div className="card-setting">
-          <div className="setting-icon">
+    <>
+      <div
+        className="card"
+        style={{
+          borderColor: backgroundColor,
+          borderStyle: color ? 'solid' : 'none'
+        }}
+        onMouseEnter={() => setIshover(true)}
+        onMouseLeave={() => setIshover(false)}
+      >
+        {ishover && (
+          <div className="card-setting" onClick={switchModal}>
             <i className="fas fa-cog"></i>
           </div>
+        )}
+        <div className="card-content">{element}</div>
+      </div>
+      <Modal
+        isOpen={opencardsetting}
+        onRequestClose={closeModal}
+        style={Style}
+      >
+        <div className="setting">
+          <button onClick={closeModal}>close</button>
         </div>
-      )}
-      <div className="card-content">{element}</div>
-    </div>
+      </Modal>
+    </>
   )
 }
 Card.propTypes = {
