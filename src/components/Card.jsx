e617a5271a939852d03b6a9b8f2cc3792e44864a
phillychi3 <phillychi3@gmail.com>
2024-04-23 12:32:21 +0800

    feat: add card setting button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types'
 import { useEffect, useState } from 'react'
 const Card = ({ element, color }) => {
   const [backgroundColor, setBackgroundColor] = useState('#ecf0f1')
+  const [ishover, setIshover] = useState(false)
 
   useEffect(() => {
     if (color) {
@@ -16,7 +17,16 @@ const Card = ({ element, color }) => {
         borderColor: backgroundColor,
         borderStyle: color ? 'solid' : 'none'
       }}
+      onMouseEnter={() => setIshover(true)}
+      onMouseLeave={() => setIshover(false)}
     >
+      {ishover && (
+        <div className="card-setting">
+          <div className="setting-icon">
+            <i className="fas fa-cog"></i>
+          </div>
+        </div>
+      )}
       <div className="card-content">{element}</div>
     </div>
   )
